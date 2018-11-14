b64d57ef52d845f9bedfd049a4d56f0e0aef3c28
Melissa Murphey <melissamurphey77@gmail.com>
2018-11-13 18:01:51 -0600

    updating

--- server.js
+++ server.js
@@ -3,16 +3,16 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = 3306;
+//var PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
 
-app.listen(process.env.PORT || 3306, function(){
-	console.log('App listening on PORT ' + process.env.PORT);
+app.listen(process.env.PORT || 3000, function(){
+	console.log('App listening on PORT 3000');
 });
